a97885b04cdfd325b7847cdd31862d84270693fe
barfa1 <barfashubham1@gmail.com>
2021-09-25 09:04:47 +0000

    reverted to a previous Calc Java and removed line adding changes

--- src/test/CalculateJava.java
+++ src/test/CalculateJava.java
@@ -5,9 +5,6 @@ public class CalculateJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Calc Java");
-		
-		//adding changes to file for tracking changes using git diff
-		System.out.println("Adding Changes ");
 
 	}
 
